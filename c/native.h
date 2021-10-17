b2f28f9bfad40443212d1ae7d20995ab43863d2d
FalsePattern <30945458+falsepattern@users.noreply.github.com>
2021-10-17 13:15:54 +0200

    stop. header time

--- c/native.h
+++ c/native.h
@@ -1,2 +1,11 @@
 #include <Windows.h>
 #include <d3d11.h>
+#include <d3d11_1.h>
+#include <d3d11_2.h>
+#include <d3d11_3.h>
+#include <d3d11_4.h>
+#include <d3d11sdklayers.h>
+#include <d3d11shader.h>
+#include <d3d11shadertracing.h>
+#include <d3dcommon.h>
+#include <d3dcsx.h>
