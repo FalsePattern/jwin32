752c474e0e6c38b6da8ab9c373b2b4c037d18e7a
FalsePattern <30945458+falsepattern@users.noreply.github.com>
2021-11-07 17:45:37 +0100

    Static wrapper methods for MemoryUtil to remove the need for grabbing the singleton.

--- src/main/java/com/falsepattern/jwin32/memory/MemoryUtil.java
+++ src/main/java/com/falsepattern/jwin32/memory/MemoryUtil.java
@@ -1,9 +1,6 @@
 package com.falsepattern.jwin32.memory;
 
-import jdk.incubator.foreign.MemoryAddress;
-import jdk.incubator.foreign.MemoryHandles;
-import jdk.incubator.foreign.MemorySegment;
-import jdk.incubator.foreign.ResourceScope;
+import jdk.incubator.foreign.*;
 
 import java.lang.invoke.VarHandle;
 import java.nio.ByteOrder;
@@ -12,7 +9,7 @@ import java.util.Map;
 
 /**
  * A generic memory utility class for reading/writing into memory segments, as well as allocating on the global memory scope for long-lived variables.
- * Make sure to {@link #free(MemorySegment)} every allocated block to avoid memory leaks, as they are not automatically released!
+ * Make sure to {@link #Free(MemorySegment)} every allocated block to avoid memory leaks, as they are not automatically released!
  */
 public class MemoryUtil implements MemoryAllocator {
     static final VarHandle VH_BYTE = MemoryHandles.varHandle(byte.class, ByteOrder.nativeOrder());
@@ -28,7 +25,7 @@ public class MemoryUtil implements MemoryAllocator {
      * @param segment The segment to read from
      * @return The value at the start of the segment
      */
-    public static byte getByte(MemorySegment segment) {
+    public static byte GetByte(MemorySegment segment) {
         return (byte) VH_BYTE.get(segment, 0);
     }
 
@@ -37,7 +34,7 @@ public class MemoryUtil implements MemoryAllocator {
      * @param segment The segment to write to
      * @param value The value to write into the segment
      */
-    public static void setByte(MemorySegment segment, byte value) {
+    public static void SetByte(MemorySegment segment, byte value) {
         VH_BYTE.set(segment, 0, value);
     }
 
@@ -46,7 +43,7 @@ public class MemoryUtil implements MemoryAllocator {
      * @param segment The segment to read from
      * @return The value at the start of the segment
      */
-    public static short getShort(MemorySegment segment) {
+    public static short GetShort(MemorySegment segment) {
         return (short) VH_SHORT.get(segment, 0);
     }
 
@@ -55,7 +52,7 @@ public class MemoryUtil implements MemoryAllocator {
      * @param segment The segment to write to
      * @param value The value to write into the segment
      */
-    public static void setShort(MemorySegment segment, short value) {
+    public static void SetShort(MemorySegment segment, short value) {
         VH_SHORT.set(segment, 0, value);
     }
 
@@ -64,7 +61,7 @@ public class MemoryUtil implements MemoryAllocator {
      * @param segment The segment to read from
      * @return The value at the start of the segment
      */
-    public static int getInt(MemorySegment segment) {
+    public static int GetInt(MemorySegment segment) {
         return (int) VH_INT.get(segment, 0);
     }
 
@@ -73,7 +70,7 @@ public class MemoryUtil implements MemoryAllocator {
      * @param segment The segment to write to
      * @param value The value to write into the segment
      */
-    public static void setInt(MemorySegment segment, int value) {
+    public static void SetInt(MemorySegment segment, int value) {
         VH_INT.set(segment, 0, value);
     }
 
@@ -82,7 +79,7 @@ public class MemoryUtil implements MemoryAllocator {
      * @param segment The segment to read from
      * @return The value at the start of the segment
      */
-    public static long getLong(MemorySegment segment) {
+    public static long GetLong(MemorySegment segment) {
         return (long) VH_LONG.get(segment, 0);
     }
 
@@ -91,7 +88,7 @@ public class MemoryUtil implements MemoryAllocator {
      * @param segment The segment to write to
      * @param value The value to write into the segment
      */
-    public static void setLong(MemorySegment segment, long value) {
+    public static void SetLong(MemorySegment segment, long value) {
         VH_LONG.set(segment, 0, value);
     }
 
@@ -100,7 +97,7 @@ public class MemoryUtil implements MemoryAllocator {
      * @param segment The segment to read from
      * @return The value at the start of the segment
      */
-    public static MemoryAddress getPointer(MemorySegment segment) {
+    public static MemoryAddress GetPointer(MemorySegment segment) {
         return MemoryAddress.ofLong((Long) VH_LONG.get(segment, 0));
     }
 
@@ -109,7 +106,7 @@ public class MemoryUtil implements MemoryAllocator {
      * @param segment The segment to write to
      * @param value The value to write into the segment
      */
-    public static void setPointer(MemorySegment segment, MemoryAddress value) {
+    public static void SetPointer(MemorySegment segment, MemoryAddress value) {
         VH_LONG.set(segment, 0, value.toRawLongValue());
     }
 
@@ -118,14 +115,171 @@ public class MemoryUtil implements MemoryAllocator {
      * @param segment The segment to release
      * @throws RuntimeException When a block is freed multiple times, or when you try to free a block not allocated by MemoryUtil.
      */
-    public void free(MemorySegment segment) {
-        if (scopes.containsKey(segment)) {
-            scopes.remove(segment).close();
+    public static void Free(MemorySegment segment) {
+        if (instance.scopes.containsKey(segment)) {
+            instance.scopes.remove(segment).close();
         } else {
             throw new RuntimeException("Tried to free already freed or unmanaged segment!");
         }
     }
 
+    public static MemorySegment Allocate(long bytesSize, long bytesAlignment) {
+        return instance.allocate(bytesSize, bytesAlignment);
+    }
+
+    public static MemorySegment MallocAligned(MemoryLayout layout) {
+        return instance.mallocAligned(layout);
+    }
+
+    public static MemorySegment Calloc(long size) {
+        return instance.calloc(size);
+    }
+
+    public static MemorySegment CallocAligned(long size, long alignment) {
+        return instance.callocAligned(size, alignment);
+    }
+
+    public static MemorySegment CallocAligned(MemoryLayout layout) {
+        return instance.callocAligned(layout);
+    }
+
+    public static MemorySegment MallocByte() {
+        return instance.mallocByte();
+    }
+
+    public static MemorySegment CallocByte() {
+        return instance.callocByte();
+    }
+
+    public static MemorySegment MallocShort() {
+        return instance.mallocShort();
+    }
+
+    public static MemorySegment CallocShort() {
+        return instance.callocShort();
+    }
+
+    public static MemorySegment MallocInt() {
+        return instance.mallocInt();
+    }
+
+    public static MemorySegment CallocInt() {
+        return instance.callocInt();
+    }
+
+    public static MemorySegment MallocLong() {
+        return instance.mallocLong();
+    }
+
+    public static MemorySegment CallocLong() {
+        return instance.callocLong();
+    }
+
+    public static MemorySegment MallocLongLong() {
+        return instance.mallocLongLong();
+    }
+
+    public static MemorySegment CallocLongLong() {
+        return instance.callocLongLong();
+    }
+
+    public static MemorySegment MallocPointer() {
+        return instance.mallocPointer();
+    }
+
+    public static MemorySegment CallocPointer() {
+        return instance.callocPointer();
+    }
+
+    public static MemorySegment ReferenceP(MemoryAddress pValue) {
+        return instance.referenceP(pValue);
+    }
+
+    public static MemorySegment ReferenceP(MemorySegment pValue) {
+        return instance.referenceP(pValue);
+    }
+
+    public static MemorySegment ToCString(String str) {
+        return instance.toCString(str);
+    }
+
+    public static MemorySegment Allocate(ValueLayout layout, byte value) {
+        return instance.allocate(layout, value);
+    }
+
+    public static MemorySegment Allocate(ValueLayout layout, char value) {
+        return instance.allocate(layout, value);
+    }
+
+    public static MemorySegment Allocate(ValueLayout layout, short value) {
+        return instance.allocate(layout, value);
+    }
+
+    public static MemorySegment Allocate(ValueLayout layout, int value) {
+        return instance.allocate(layout, value);
+    }
+
+    public static MemorySegment Allocate(ValueLayout layout, float value) {
+        return instance.allocate(layout, value);
+    }
+
+    public static MemorySegment Allocate(ValueLayout layout, long value) {
+        return instance.allocate(layout, value);
+    }
+
+    public static MemorySegment Allocate(ValueLayout layout, double value) {
+        return instance.allocate(layout, value);
+    }
+
+    public static MemorySegment Allocate(ValueLayout layout, Addressable value) {
+        return instance.allocate(layout, value);
+    }
+
+    public static MemorySegment AllocateArray(ValueLayout elementLayout, byte[] array) {
+        return instance.allocateArray(elementLayout, array);
+    }
+
+    public static MemorySegment AllocateArray(ValueLayout elementLayout, short[] array) {
+        return instance.allocateArray(elementLayout, array);
+    }
+
+    public static MemorySegment AllocateArray(ValueLayout elementLayout, char[] array) {
+        return instance.allocateArray(elementLayout, array);
+    }
+
+    public static MemorySegment AllocateArray(ValueLayout elementLayout, int[] array) {
+        return instance.allocateArray(elementLayout, array);
+    }
+
+    public static MemorySegment AllocateArray(ValueLayout elementLayout, float[] array) {
+        return instance.allocateArray(elementLayout, array);
+    }
+
+    public static MemorySegment AllocateArray(ValueLayout elementLayout, long[] array) {
+        return instance.allocateArray(elementLayout, array);
+    }
+
+    public static MemorySegment AllocateArray(ValueLayout elementLayout, double[] array) {
+        return instance.allocateArray(elementLayout, array);
+    }
+
+    public static MemorySegment AllocateArray(ValueLayout elementLayout, Addressable[] array) {
+        return instance.allocateArray(elementLayout, array);
+    }
+
+    public static MemorySegment Allocate(MemoryLayout layout) {
+        return instance.allocate(layout);
+    }
+
+    public static MemorySegment AllocateArray(MemoryLayout elementLayout, long count) {
+        return instance.allocateArray(elementLayout, count);
+    }
+
+    public static MemorySegment Allocate(long bytesSize) {
+        return instance.allocate(bytesSize);
+    }
+
+    @Override
     public MemorySegment mallocAligned(long size, long alignment) {
         var scope = ResourceScope.newConfinedScope();
         var segment = MemorySegment.allocateNative(size, alignment, scope);
